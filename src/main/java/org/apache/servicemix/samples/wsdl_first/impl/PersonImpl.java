22fdb5ecb6aa898e79009aa7cb8d1148f22a603f
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-10-22 11:26:58 +0200

    first version

---
+++ src/main/java/org/apache/servicemix/samples/wsdl_first/impl/PersonImpl.java
@@ -0,0 +1,33 @@
+package org.apache.servicemix.samples.wsdl_first.impl;
+
+import java.util.logging.Logger;
+
+import org.apache.servicemix.samples.wsdl_first.Person;
+import org.apache.servicemix.samples.wsdl_first.UnknownPersonFault;
+
+/**
+ * This is an example.
+ */
+@javax.jws.WebService(serviceName = "PersonService", portName = "soap", targetNamespace = "http://servicemix.apache.org/samples/wsdl-first", endpointInterface = "org.apache.servicemix.samples.wsdl_first.Person")
+public class PersonImpl implements Person {
+	private static final Logger LOG = Logger.getLogger(PersonImpl.class.getName());
+
+	public void getPerson(javax.xml.ws.Holder<java.lang.String> personId,
+			javax.xml.ws.Holder<java.lang.String> ssn,
+			javax.xml.ws.Holder<java.lang.String> name)
+			throws UnknownPersonFault {
+		LOG.info("Executing operation getPerson");
+		System.out.println(personId.value);
+		try {
+			java.lang.String ssnValue = "";
+			ssn.value = ssnValue;
+			java.lang.String nameValue = "";
+			name.value = nameValue;
+		} catch (java.lang.Exception ex) {
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+		// throw new UnknownPersonFault("UnknownPersonFault...");
+	}
+
+}
